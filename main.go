991b4d5b98709ae49b2836e002573412fbfe40cf
Nikita Lebedev <lebedev@greensight.ru>
2022-09-04 19:16:47 +0300

    subscribers

--- main.go
+++ main.go
@@ -8,12 +8,13 @@ import (
 	"cryptobot/internal/scheduler"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
+	"os"
 	"time"
 )
 
 //scp -P 3333 main root@185.237.218.45:/
 func main() {
-	bot, err := tgbotapi.NewBotAPI("1968504691:AAGjdJLcvP9LOspNEWClbboFlFk5irhLnfs")
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
 	if err != nil {
 		log.Panicln(err)
 	}
